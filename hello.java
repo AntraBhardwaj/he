94a7bf438ab5fc3e243d0b8bbb309a25177a1906
AntraBhardwaj <42044628+AntraBhardwaj@users.noreply.github.com>
2018-08-03 08:30:45 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,17 @@
+package number;
+import java.util.*;
+public class Checkchar 
+{
+    public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int N,i;
+		Scanner s= new Scanner(System.in);
+		N = s.nextInt();	
+		s.close();
+	   for(i=1;i<=N;i++)
+	   {
+		   System.out.println("Hello");
+	   }
+  }	
+}
